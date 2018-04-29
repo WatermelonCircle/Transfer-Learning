724d197301de66f9bacb1e7c8225829f050ec449
Shun Su <Shun_Su@Shuns-MacBook-Pro-4.local>
2018-04-29 14:40:08 -0700

    feature-extraction updated

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,5 +1,9 @@
 import pickle
+import keras
 import tensorflow as tf
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten
+import numpy as np
 # TODO: import Keras layers you need here
 
 flags = tf.app.flags
@@ -8,7 +12,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
-
+flags.DEFINE_integer('epochs', 40, "The number of epochs.")
+flags.DEFINE_integer('batch_size', 128, "The batch size.")
 
 def load_bottleneck_data(training_file, validation_file):
     """
@@ -40,15 +45,30 @@ def main(_):
 
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
-
+    
+    from sklearn.preprocessing import LabelBinarizer
+    label_binarizer = LabelBinarizer()
+    y_one_hot = label_binarizer.fit_transform(y_train)
+    y_one_hot_val = label_binarizer.fit_transform(y_val)
+    #y_train = keras.utils.to_categorical(y_train, num_classes)
+    #y_val= keras.utils.to_categorical(y_val, num_classes)
     # TODO: define your model and hyperparams here
+    
+    
     # make sure to adjust the number of classes based on
     # the dataset
+    nb_class=len(np.unique(y_train))
+    model = Sequential()
+    model.add(Flatten(input_shape=(X_train.shape[1:])))
+    model.add(Dense(nb_class,activation='softmax'))
+    print(X_train.shape[1:])
+    
     # 10 for cifar10
     # 43 for traffic
 
     # TODO: train your model here
-
+    model.compile('adam', 'categorical_crossentropy', ['accuracy'])
+    history = model.fit(X_train, y_one_hot, nb_epoch=FLAGS.epochs,batch_size=FLAGS.batch_size,validation_data=(X_val, y_one_hot_val),shuffle=True)
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
